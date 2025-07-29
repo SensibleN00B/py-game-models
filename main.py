212ec9d12dcae17004ad02bc387a55d8f84728d8
SensibleN00B <sensible.noob@gmail.com>
2025-07-29 18:48:46 +0300

    Added models Race, Skill, Guild, Player and logic in main.py

--- main.py
+++ main.py
@@ -1,10 +1,43 @@
+import json
+
 import init_django_orm  # noqa: F401
 
 from db.models import Race, Skill, Player, Guild
 
 
 def main() -> None:
-    pass
+    with open("players.json", "r") as file:
+        players_info = json.load(file)
+
+    for name, info in players_info.items():
+        race_info = info["race"].copy()
+        skills_info = race_info.pop("skills")
+
+        race, _ = Race.objects.get_or_create(
+            **race_info,
+        )
+
+        for skill_ in skills_info:
+            Skill.objects.get_or_create(
+                race=race,
+                **skill_,
+            )
+
+        guild = None
+        if info.get("guild"):
+            guild, _ = Guild.objects.get_or_create(
+                **info["guild"]
+            )
+
+        Player.objects.get_or_create(
+            nickname=name,
+            defaults={
+                "email": info["email"],
+                "bio": info["bio"],
+                "race": race,
+                "guild": guild
+            }
+        )
 
 
 if __name__ == "__main__":
