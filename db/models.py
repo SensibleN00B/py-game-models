446a7f12422942cced1da17fdd788301778cd215
SensibleN00B <sensible.noob@gmail.com>
2025-07-29 21:14:39 +0300

    Added blank=True to bio field in Player model

--- db/models.py
+++ db/models.py
@@ -33,7 +33,7 @@ class Guild(models.Model):
 class Player(models.Model):
     nickname = models.CharField(unique=True, max_length=255)
     email = models.EmailField(max_length=255, unique=False)
-    bio = models.CharField(max_length=255)
+    bio = models.CharField(max_length=255, blank=True)
     race = models.ForeignKey(
         Race,
         on_delete=models.CASCADE,
